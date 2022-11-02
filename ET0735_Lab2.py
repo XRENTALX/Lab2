ff9f684bfa67a32be332674fe5f9571cdf6281fa
xrentalx1 <shengkai.21@ichat.sp.edu.sg>
2022-11-02 22:51:30 +0800

    Modified Lab2.py

--- ET0735_Lab2.py
+++ ET0735_Lab2.py
@@ -1,1 +1,36 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+
+
+def display_main_menu():
+    print("display_main_menu")
+    print("Enter some numbers separated by commas (e.g. 5, 67,32)")
+
+
+def get_user_input():
+    print("get_user_input")
+
+
+def calc_average():
+    print("calc_average")
+
+
+def find_min_max():
+    print("find_min_max")
+
+
+def sort_temperature():
+    print(sort_temperature)
+
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
