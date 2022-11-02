e68fa676065eb4ad47b1aaa331ffd53aaceab875
xrentalx1 <shengkai.21@ichat.sp.edu.sg>
2022-11-02 23:49:58 +0800

    Finished calc_average function

--- ET0735_Lab2.py
+++ ET0735_Lab2.py
@@ -2,19 +2,25 @@ def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     num_list = get_user_input()
+    calc_average(num_list)
 
 
 def display_main_menu():
-    print("display_main_menu")
     print("Enter some numbers separated by commas (e.g. 5, 67,32)")
 
 
 def get_user_input():
-    print("get_user_input")
+    num = input()
+    num_split = num.split()
+    num_list = num_split
+    num_list = list(map(float, num_list))
+    return num_list
 
 
-def calc_average():
+def calc_average(num_list):
     print("calc_average")
+    num_average = sum(num_list)/len(num_list)
+    print(num_average)
 
 
 def find_min_max():
