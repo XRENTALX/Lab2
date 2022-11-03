5c6e9a82b7a5fa13e819e0d840154f0f2320e974
xrentalx1 <shengkai.21@ichat.sp.edu.sg>
2022-11-03 16:11:32 +0800

    Corrected error

--- bmi.py
+++ bmi.py
@@ -8,10 +8,13 @@ def calculate_bmi(height, weight):
 
     if bmi < 18.5:
         print("Under Weight")
-    elif bmi >= 18.5 or bmi <= 25.0:
+        return -1
+    elif bmi >= 18.5 and bmi <= 25.0:
         print("Normal Weight")
+        return 0
     else:
         print("Overweight")
+        return 1
 
 
-calculate_bmi(1.73, 57)
+calculate_bmi(1.65, 45)
